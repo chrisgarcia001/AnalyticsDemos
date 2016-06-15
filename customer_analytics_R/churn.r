7a409ad43a908938cedca5025124a2af430cda7b
unknown <chrisgarcia001@gmail.com>
2016-06-15 14:27:51 -0400

    Initial commit

---
+++ customer_analytics_R/churn.r
@@ -0,0 +1,29 @@
+library(caret)
+set.seed(25714)
+
+# Read in data
+dataset <- read.csv("data/churn_data.csv")
+
+# Separate data into training and test sets 
+trainp <- 0.7
+inTrain <- createDataPartition(y = dataset$Churn, p = trainp, list = FALSE)
+training <- dataset[inTrain,]
+testing <- dataset[-inTrain,]
+
+# Define the model structure
+model <- Churn ~ Gender + Age + Income + FamilySize + Education + Calls + Visits
+
+# Fit the model
+message("Model Fitting...")
+start.time <- Sys.time()
+modelFit <- train(model, data=training, method="rf")
+end.time <- Sys.time()
+message("Done!")
+message(paste("Elapsed Time (sec):", end.time - start.time))
+message("")
+
+# Predict churn on test data
+predictions <- predict(modelFit,newdata=testing)
+
+# Evaluate and print the quality of prediction on test set
+print(confusionMatrix(predictions,testing$Churn))
---
+++ customer_analytics_R/segment.r
@@ -0,0 +1,29 @@
+library(mclust)
+library(cluster)
+
+# Load data
+dataset = read.csv("data/purchases.csv", row.names=1)
+
+# Build latent cluster model and show results
+fit <- Mclust(dataset)
+print(fit$BIC) # Show the fit summary
+classif <- fit$classification # The segment classification vector - class of each person
+
+# Build classification frame
+classifications <- cbind(classif)
+print(classifications) # Show the classes of each person
+rownames(classifications) <- rownames(dataset)
+
+fitdata <- cbind(dataset, classif=classif) # Add the classes to the data as a new column
+
+# Display cluster plot based on two latent components and show cross-var plots
+plot(fitdata)
+dev.new()
+clusplot(dataset, classifications, color=TRUE, shade=TRUE, labels=2, lines=0)
+
+
+
+
+
+
+
